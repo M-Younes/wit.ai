de1b14e01376ede5f3334541fdbf78b1b9d18c8f
Younes <m.younes87@gmail.com>
2016-05-01 18:53:01 +0800

    test fb webhook

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,9 @@ class MessagesController < ApplicationController
 	def index
 		render json: "agent", status: 200
 	end
+
+	def webhook
+		Rails.logger.info "<<<<<<<<#{params.inspect}"
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'messages#index'
+  get 'webhook' => 'messages#webhook', as: :webhook
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
