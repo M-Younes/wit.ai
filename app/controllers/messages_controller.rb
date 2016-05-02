b3ed7634e3b740f321b75a3b882153a3d4cf5e5e
Younes <m.younes87@gmail.com>
2016-05-02 12:54:45 +0800

    add fb webhook

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,14 @@ class MessagesController < ApplicationController
 	def index
 		render json: "agent", status: 200
 	end
+
+	def webhook
+		challenge = params["hub.challenge"] 
+		if params["hub.verify_token"].to_s == Digest::SHA1.hexdigest(Settings.token)
+			render :json => { :params["hub.challenge"] => challenge}, :status => 200
+		else
+			render :json => { :error => "Error, wrong validation token" }, :status => 406
+		end
+	end
+
 end
