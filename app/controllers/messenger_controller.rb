1e39b84f07b2eca9ba912a8b4416cab8c5b50cb9
Younes <m.younes87@gmail.com>
2016-08-24 15:15:47 +0800

    use Messenger client to send text msg

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,5 +1,6 @@
 class MessengerController < Messenger::MessengerController
 
+
 	def webhook
 		messages = params["entry"].first["messaging"]
 		sender_id = messages.first["sender"]["id"]
@@ -7,10 +8,18 @@ class MessengerController < Messenger::MessengerController
 			messages.each do |event|
 				if event["message"] && event["message"]["text"]
 					text = event["message"]["text"].to_s
-					FacebookBot.new.send_text_message(sender_id, "This is a bot testing. Echo " + "#{text}")
+					send_message(sender_id,text)
 				end
 			end
 		end
 		render :nothing => true, :status => 200		
+	end
+
+
+	def send_message(user_id, message)
+	  Messenger::Client.send(
+	    Messenger::Request.new(
+	      Messenger::Elements::Text.new(text: 'Testing bot echo '+'message'),user_id)
+	  )		
 	end
 end
