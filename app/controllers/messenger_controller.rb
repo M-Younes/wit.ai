fab4d99594d354d56d92b023b54aa96fce3a5e90
Younes <m.younes87@gmail.com>
2016-08-24 14:46:56 +0800

    add webhook action

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,2 +1,16 @@
 class MessengerController < Messenger::MessengerController
+
+	def webhook
+		messages = params["entry"].first["messaging"]
+		sender_id = messages.first["sender"]["id"]
+		unless messages.length.zero?
+			messages.each do |event|
+				if event["message"] && event["message"]["text"]
+					text = event["message"]["text"].to_s
+					FacebookBot.new.send_text_message(sender_id, "This is a bot testing. Echo " + "#{text}")
+				end
+			end
+		end
+		render :nothing => true, :status => 200		
+	end
 end
